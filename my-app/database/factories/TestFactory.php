2e714a4bbc84bca8a2b320a1b8a5dbb150fdc791
Jeiji <jeiji@me.com>
2021-06-19 21:43:15 +0900

    変更記録、試行成功…多分 Recording changes. Trial successful... probably.

---
+++ my-app/database/factories/TestFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\test;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TestFactory extends Factory
+{
+    protected $model = test::class;
+
+    public function definition(): array
+    {
+    	return [
+    	    //
+    	];
+    }
+}
