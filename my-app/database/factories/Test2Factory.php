4d83b0f61a3dc5b7aa3acb75533a0403d3104d24
Jeiji <jeiji@me.com>
2021-06-19 22:02:01 +0900

    停車 Pitstop

---
+++ my-app/database/factories/Test2Factory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\test2;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class Test2Factory extends Factory
+{
+    protected $model = test2::class;
+
+    public function definition(): array
+    {
+    	return [
+    	    //
+    	];
+    }
+}
